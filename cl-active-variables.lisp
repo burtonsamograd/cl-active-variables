fa29f76f8832ac82a08ccbf4885f78baadce4fe3
Burton Samograd <burton.samograd@gmail.com>
2015-01-25 14:23:43 -0700

    Use once-only on macro arguments. Rename var to symbol.

--- cl-active-variables.lisp
+++ cl-active-variables.lisp
@@ -33,11 +33,14 @@
 ;; 200
 ;;
 ;; Author: Burton Samograd <burton.samograd@gmail.com>
-;; Year: 2012
+;; Year: 2015
 
-(defpackage :active-variables
+(eval-when (:compile-toplevel :load-toplevel)
+  (require :alexandria))
+
+(defpackage :cl-active-variables
   (:use :cl)
-  (:nicknames "ACTIVE")
+  (:nicknames "ACTIVE-VARIABLES")
   (:export "DEFACTIVE" "SETACTIVE"))
 (in-package :active-variables)
 
@@ -56,49 +59,50 @@
 			    (intern (format nil "$~A" v)))))
 		    t)
 
-(defmacro defactive (var value &key write-handler read-handler)
+(defmacro defactive (symbol value &key write-handler read-handler)
   "Define an active variable.  An active variable is one which calls a
 function when read or written to.  To read an active variable and call
 it's read handler, place a $ before it's name.  To write an active
-variable and call it's write handler, use (setf $var ...).  To read
+variable and call it's write handler, use (setf $symbol ...).  To read
 and set the variable without calling any handler functions, do not use
 the previx $ character.  The variable should be defined without the $
 prefix, like so (defactive x ...) and read using $x and written to
 using (setf $x ...).  You can define handlers using this function with
 the :read-handler and :write-handler keywords, or using setactive."
- (let ((setf-handler-name (gensym)))
-  `(progn
-     ;; add the variable to the active variables list
-     (setf (gethash ',var *active-variables*) t)
-     ;; declare the var and the value
-     (defparameter ,var ,value)
-     ;; define the setf handler name which wrapped around the variable
-     ;; when it is is read using $var notation
-     (defmacro ,setf-handler-name ()
-       (let ((read-handler (gensym)))
-         `(let ((,read-handler (get ',',var :read-handler)))
-            (if ,read-handler
-                (funcall ,read-handler ,',var)
-                ,',var))))
-     ;; define a handler to call the write handler for when you do a
-     ;; (setf $var ...)
-     (defsetf ,setf-handler-name () (new-val)
-       (let ((write-handler (gensym)))
-         `(let ((,write-handler (get ',',var :write-handler)))
-            (when ,write-handler
-              (setf ,',var (funcall ,write-handler ,',var ,new-val))))))
-     ;; save all the info in the variable
-     (setf (get ',var :setf-handler-name) ',setf-handler-name)
-     (setf (get ',var :write-handler) ,write-handler)
-     (setf (get ',var :read-handler) ,read-handler)
-     ;; return the value
-     ,value)))
-
-(defmacro setactive (var &key read-handler write-handler)
+  (let ((setf-handler-name (gensym)))
+    (alexandria:once-only (value write-handler read-handler)
+      `(progn
+	 ;; add the variable to the active variables list
+	 (setf (gethash ',symbol *active-variables*) t)
+	 ;; declare the variable and the value
+	 (defparameter ,symbol ,value)
+	 ;; define the setf handler name which wrapped around the variable
+	 ;; when it is is read using $var notation
+	 (defmacro ,setf-handler-name ()
+	   (let ((read-handler (gensym)))
+	     `(let ((,read-handler (get ',',symbol :read-handler)))
+	      (if ,read-handler
+		  (funcall ,read-handler ,',symbol)
+		  ,',symbol))))
+	 ;; define a handler to call the write handler for when you do a
+	 ;; (setf $symbol ...)
+	 (defsetf ,setf-handler-name () (new-val)
+	   (let ((write-handler (gensym)))
+	     `(let ((,write-handler (get ',',symbol :write-handler)))
+		(when ,write-handler
+		  (setf ,',symbol (funcall ,write-handler ,',symbol ,new-val))))))
+	 ;; save all the info in the variable
+	 (setf (get ',symbol :setf-handler-name) ',setf-handler-name)
+	 (setf (get ',symbol :write-handler) ,write-handler)
+	 (setf (get ',symbol :read-handler) ,read-handler)
+	 ;; return the value
+	 ,value))))
+  
+(defmacro setactive (symbol &key read-handler write-handler)
   "helper function for setting :read-handler and :write-handler of the
 active variable var"
   `(progn
      (when ,read-handler
-       (setf (get ',var :read-handler) ,read-handler))
+       (setf (get ',symbol :read-handler) ,read-handler))
      (when ,write-handler
-       (setf (get ',var :write-handler) ,write-handler))))
+       (setf (get ',symbol :write-handler) ,write-handler))))
